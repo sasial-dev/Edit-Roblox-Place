c9610a5ea2bc233d18376bb99ff2d32a090c7cef
sasial-roblox <44125644+sasial-dev@users.noreply.github.com>
2021-04-08 14:39:51 +1000

    Uninstall Script Updates

--- src/uninstall.js
+++ src/uninstall.js
@@ -6,11 +6,12 @@ const resolve = require('resolve-dir')
 const questions = [{
   type: 'confirm',
   name: 'configFile',
-  message: 'Would you like edit-roblox-place to your configuration file at ~/.edit-roblox-place/config.json?'
+  message: 'Would you like edit-roblox-place to remove your configuration file at ~/.edit-roblox-place/config.json?'
 }]
 
 if (fs.existsSync((resolve('~/.edit-roblox-place/config.json')))) {
   console.log(chalk.bold("Thank you for installing edit-roblox-place. We're sorry to see you go."))
+  console.log(`Due to node limitations, the uninstall script is ${chalk.underline("ran during an update")}. If you are updating and not uninstalling, please opt to ${chalk.redBright("keep")} your config file.`)
   inquirer.prompt(questions).then((answers) => {
     if (answers.configFile === true) {
       console.log(chalk.bold('Removing the config file.'))
